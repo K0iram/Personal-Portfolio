95dcf76ffed10da7e7c6784e1b0852acc5bbd171
mario7746 <m.kennedykavouras@gmail.com>
2017-05-23 23:19:59 -0400

    adds react scroll anchor package. Adds scrollable anchor to home page '

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react'
+import ScrollableAnchor, {configureAnchors} from 'react-scrollable-anchor'
+
 import './style.css'
 
 
 class Home extends Component {
-
-  constructor(props) {
-  super(props)
-  this.state = {
-  }
   
-  this.handleScroll = this.handleScroll.bind(this)
- 
+  componentDidMount() {
+    configureAnchors({keepLastAnchorHash: false, scrollDuration: 1000})
   }
-handleScroll(event) {
-  event.preventDefault()
-  document.querySelector('.description').scrollIntoView();
-}
 
   render() {
     return (
@@ -25,13 +18,14 @@ handleScroll(event) {
           <h1>Mario Kennedy</h1>
           <h5>WEB DEVELOPER</h5>
 
-          <a onClick={this.handleScroll}><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+          <a href="#description"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
           <title>chevron-thin-down</title>
           <path d="M17.418 6.109c0.272-0.268 0.709-0.268 0.979 0s0.271 0.701 0 0.969l-7.908 7.83c-0.27 0.268-0.707 0.268-0.979 0l-7.908-7.83c-0.27-0.268-0.27-0.701 0-0.969s0.709-0.268 0.979 0l7.419 7.141 7.418-7.141z"></path>
           </svg></a>
 
           </div>
         </div>
+        <ScrollableAnchor id='description'>
         <div className="description">
           <br/>
           <h5>ABOUT ME</h5>
@@ -42,6 +36,7 @@ handleScroll(event) {
           people that have a vision of changing the world.
           </p>
         </div>
+        </ScrollableAnchor>
       </div>
     )
   }
